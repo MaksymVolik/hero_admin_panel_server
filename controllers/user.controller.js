b20dbfd16aeed5e84069bfeeae998691ef49f53e
Maksym Volik <maksymvolik@gmail.com>
2024-03-21 00:45:21 +0200

    create environment variables TOKEN_EXPIRES_IN

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,7 +17,7 @@ const refreshTokenCookieOptions = {
   ),
   maxAge: process.env.REFRESH_TOKEN_EXPIRES_IN * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "None",
 };
 
 if (process.env.NODE_ENV === "production")
